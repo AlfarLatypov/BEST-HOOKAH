fe53523ff7be33b864fcf4c44fb98280cc5b1562
Альфар Латыпов <@local.itstep>
2019-01-14 21:36:45 +0600

    Добавьте файлы проекта.

---
+++ HotelAtr.DAL/RoomType.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace HotelAtr.DAL
+{
+    [Table("RoomType", Schema = "Table")]
+    public class RoomType
+    {
+        [Key, DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int RoomTypeId { get; set; }
+
+        [StringLength(2000)]
+        public string Description { get; set; }
+
+        public List<Room> Room { get; set; }
+    }
+}
